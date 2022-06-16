87b95399951f17c6acb65ca0ba57bd90eb4d0edc
Rixiao Zhang <75501942+rixiaozhang@users.noreply.github.com>
2022-06-15 17:04:52 -0700

    Update FlashloanGuard.sol

--- FlashloanGuard.sol
+++ FlashloanGuard.sol
@@ -9,13 +9,17 @@ abstract contract FlashloanGuard {
 
     constructor() {}
     
+    //When a fund is deposited into a smart contract, 
+    //it stores the address that started the transaction and the current block number. 
     modifier entrance() {
         _customer = tx.origin;
         _blockNumber = block.number;
         
         _;
     }
-
+    
+    //When a fund is being withdrawn, it checks the address that sent the transaction against the stored address. 
+    //If these two addresses match, the current block number must be strictly greater than the stored block number. 
     modifier nonFlashloan() {
         if (_customer == tx.origin){
             require(_blockNumber < block.number, "FlashloanGuard: potential flashloan attack");
